7a905beb1bc5441d9a7fe586d83091fe6042493a
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-05-06 11:17:03 -0300

    #feature/16 - Implementação de testes unitários para LoginComponent e RegisterComponent

--- src/app/features/account/register/register.component.spec.ts
+++ src/app/features/account/register/register.component.spec.ts
@@ -1,25 +1,28 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
 import { AccountService } from '../services/account.service';
 import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { 
   DEFAULT_LANGUAGE, 
-  MissingTranslationHandler, 
-  TranslateCompiler, 
-  TranslateLoader, 
   TranslateModule, 
-  TranslateParser, 
   TranslateService, 
-  TranslateStore, 
   USE_DEFAULT_LANG, USE_EXTEND, USE_STORE 
 } from '@ngx-translate/core';
+import { of, throwError } from 'rxjs';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let registerSpy: jasmine.Spy;
+  let loginSpy: jasmine.Spy;
+  let accountService: AccountService;
+
+  let toastrService: ToastrService
+  let showSuccess: jasmine.Spy;
+  let showError: jasmine.Spy;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -49,7 +52,251 @@ describe('RegisterComponent', () => {
     fixture.detectChanges();
   });
 
+  beforeEach(() => {
+    accountService = TestBed.get(AccountService);
+    registerSpy = spyOn(accountService, 'registerUser');
+    loginSpy = spyOn(accountService, 'login');
+
+    toastrService = TestBed.get(ToastrService);
+    showSuccess = spyOn(toastrService, 'success');
+    showError = spyOn(toastrService, 'error');
+  });
+
+  const REGISTER_USER = {
+    userName: 'test@teste.com',
+    password: '123456',
+    confirmPassword: '123456',
+    role: 'ADMIN'
+  }
+
+  const USER_TOKEN = {
+    login: 'test@teste.com',
+    token: 'abc123'
+  }
+
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should register successfully', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('123456');
+    registerForm.get('role').setValue('ADMIN');
+    registerForm.markAsDirty();
+    registerForm.valid;
+
+    registerSpy.and.callFake((options: any) => {
+      expect(options.userName).toEqual('test@teste.com');
+      expect(options.password).toEqual('123456');
+      expect(options.confirmPassword).toEqual('123456');
+      expect(options.role).toEqual('ADMIN');
+      return of(REGISTER_USER);
+    });
+
+    loginSpy.and.callFake((options: any) => {
+      expect(options.login).toEqual('test@teste.com');
+      expect(options.password).toEqual('123456');
+      return of(USER_TOKEN);
+    });
+    
+    fixture.detectChanges();
+    component.addAccount();
+
+    expect(registerSpy).toHaveBeenCalled();
+    expect(loginSpy).toHaveBeenCalled();
+    expect(showSuccess).toHaveBeenCalledWith('br_com_supermarket_REGISTER_SUCCESSFUL');
+  });
+
+  it('should not register user when there is an error in registration', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('123456');
+    registerForm.get('role').setValue('ADMIN');
+    registerForm.markAsDirty();
+    registerForm.valid;
+
+    const errors = {
+      error: {
+        errors: [
+          'Teste'
+        ]
+      }
+    }
+
+    registerSpy.and.returnValue(throwError(errors));
+    
+    fixture.detectChanges();
+    component.addAccount();
+
+    expect(registerSpy).toHaveBeenCalled();
+    expect(showError).toHaveBeenCalledWith('br_com_supermarket_AN_ERROR_OCCURRED_WHILE_REGISTERING');
+  });
+
+  it('should not authenticate user when there is an error in authentication proccess', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('123456');
+    registerForm.get('role').setValue('ADMIN');
+    registerForm.markAsDirty();
+    registerForm.valid;
+
+    const errors = {
+      error: {
+        errors: [
+          'Teste'
+        ]
+      }
+    }
+
+    registerSpy.and.callFake((options: any) => {
+      expect(options.userName).toEqual('test@teste.com');
+      expect(options.password).toEqual('123456');
+      expect(options.confirmPassword).toEqual('123456');
+      expect(options.role).toEqual('ADMIN');
+      return of(REGISTER_USER);
+    });
+
+    loginSpy.and.returnValue(throwError(errors));
+    
+    fixture.detectChanges();
+    component.addAccount();
+
+    expect(registerSpy).toHaveBeenCalled();
+    expect(loginSpy).toHaveBeenCalled();
+    expect(showError).toHaveBeenCalledWith('br_com_supermarket_AN_ERROR_OCCURRED_WHILE_REGISTERING');
+  });
+
+  it('should required user', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('123456');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('userName').valid).toEqual(false);
+    expect(component.validationMessages.userName.required).toEqual('br_com_supermarket_INFORM_THE_USER');
+  });
+
+  it('should invalidate the form when login is incorrect', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('/*&!s');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('123456');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('userName').valid).toEqual(false);
+    expect(component.validationMessages.userName.userName).toEqual('br_com_supermarket_INVALID_USER');
+  });
+
+  it('should required password', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('');
+    registerForm.get('confirmPassword').setValue('');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('password').valid).toEqual(false);
+    expect(component.validationMessages.password.required).toEqual('br_com_supermarket_INFORM_THE_PASSWORD');
+  });
+
+  it('should invalidate password when rangeLength is less than 6 characters', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('1');
+    registerForm.get('confirmPassword').setValue('1');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('password').valid).toEqual(false);
+    expect(component.validationMessages.password.rangeLength).toEqual('br_com_supermarket_PASSWORD_CANNOT_BE_LESS_THAN_6_AND_GREATER_THAN_8');
+  });
+
+  it('should invalidate password when rangeLength is greater than 8 characters', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456789');
+    registerForm.get('confirmPassword').setValue('123456789');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('password').valid).toEqual(false);
+    expect(component.validationMessages.password.rangeLength).toEqual('br_com_supermarket_PASSWORD_CANNOT_BE_LESS_THAN_6_AND_GREATER_THAN_8');
+  });
+
+  it('should required confirmPassword', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('confirmPassword').valid).toEqual(false);
+    expect(component.validationMessages.confirmPassword.required).toEqual('br_com_supermarket_INFORM_THE_PASSWORD_AGAIN');
+  });
+
+  it('should invalidate confirmPassword when rangeLength is less than 6 characters', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('1');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('confirmPassword').valid).toEqual(false);
+    expect(component.validationMessages.confirmPassword.rangeLength).toEqual('br_com_supermarket_PASSWORD_CANNOT_BE_LESS_THAN_6_AND_GREATER_THAN_8');
+  });
+
+  it('should invalidate confirmPassword when rangeLength is greater than 8 characters', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('123456789');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('confirmPassword').valid).toEqual(false);
+    expect(component.validationMessages.confirmPassword.rangeLength).toEqual('br_com_supermarket_PASSWORD_CANNOT_BE_LESS_THAN_6_AND_GREATER_THAN_8');
+  });
+
+  it('should invalidate when password is different of confirmPassword', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('1234567');
+    registerForm.get('role').setValue('ADMIN');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('confirmPassword').valid).toEqual(false);
+    expect(component.validationMessages.confirmPassword.equalTo).toEqual('br_com_supermarket_PASSWORD_DONOT_MATCH');
+  });
+
+  it('should required role', () => {
+    const registerForm: FormGroup = component.registerForm;
+    registerForm.get('userName').setValue('test@teste.com');
+    registerForm.get('password').setValue('123456');
+    registerForm.get('confirmPassword').setValue('1234567');
+    registerForm.get('role').setValue('');
+    
+    component.ngAfterViewInit();
+
+    expect(registerForm.get('role').valid).toEqual(false);
+    expect(component.validationMessages.role.required).toEqual('br_com_supermarket_ENTER_USE_ROLE');
   });
 });
