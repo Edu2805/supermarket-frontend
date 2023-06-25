0667b5f13cc0c96998603301f960831591643965
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-06-25 11:26:49 -0300

    #feature/31 - Implementação do read de department

--- src/app/features/department/crud/read/read-department.component.spec.ts
+++ src/app/features/department/crud/read/read-department.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReadDepartmentComponent } from './read-department.component';
+import { DepartmentService } from '../../services/department.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('ReadDepartmentComponent', () => {
   let component: ReadDepartmentComponent;
@@ -8,7 +12,23 @@ describe('ReadDepartmentComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReadDepartmentComponent ]
+      declarations: [ 
+        ReadDepartmentComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        DepartmentService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
