7efdd44d7dc6867af0f9d23dfe0ccdc7f1711c76
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-07-02 18:02:41 -0300

    #feature/38 - Implementação do Create e Update de Main Section

--- src/app/features/mainsection/crud/create-mainsection/create-mainsection.component.spec.ts
+++ src/app/features/mainsection/crud/create-mainsection/create-mainsection.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateMainsectionComponent } from './create-mainsection.component';
+import { MainsectionService } from '../../services/mainsection.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
+import { EstablishmentService } from 'src/app/features/establishment/services/establishment.service';
 
 describe('CreateMainsectionComponent', () => {
   let component: CreateMainsectionComponent;
@@ -8,7 +13,24 @@ describe('CreateMainsectionComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateMainsectionComponent ]
+      declarations: [ 
+        CreateMainsectionComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        MainsectionService,
+        EstablishmentService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
--- src/app/features/mainsection/crud/update-mainsection/update-mainsection.component.spec.ts
+++ src/app/features/mainsection/crud/update-mainsection/update-mainsection.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UpdateMainsectionComponent } from './update-mainsection.component';
+import { MainsectionService } from '../../services/mainsection.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
+import { EstablishmentService } from 'src/app/features/establishment/services/establishment.service';
 
 describe('UpdateMainsectionComponent', () => {
   let component: UpdateMainsectionComponent;
   let fixture: ComponentFixture<UpdateMainsectionComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UpdateMainsectionComponent ]
+      declarations: [ 
+        UpdateMainsectionComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        MainsectionService,
+        EstablishmentService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
