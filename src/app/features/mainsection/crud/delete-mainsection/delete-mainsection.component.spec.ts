e1179a8f475a2a75bff1a3791d9965b1829152f5
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-07-02 16:54:49 -0300

    #feature/37 - Implementação das funcionalidades de delete de main section

--- src/app/features/mainsection/crud/delete-mainsection/delete-mainsection.component.spec.ts
+++ src/app/features/mainsection/crud/delete-mainsection/delete-mainsection.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteMainsectionComponent } from './delete-mainsection.component';
+import { MainsectionService } from '../../services/mainsection.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('DeleteMainsectionComponent', () => {
   let component: DeleteMainsectionComponent;
   let fixture: ComponentFixture<DeleteMainsectionComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DeleteMainsectionComponent ]
+      declarations: [ 
+        DeleteMainsectionComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        MainsectionService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ],
     })
     .compileComponents();
 
--- src/app/features/mainsection/crud/delete-mainsection/delete-mainsection.component.ts
+++ src/app/features/mainsection/crud/delete-mainsection/delete-mainsection.component.ts
@@ -1,4 +1,11 @@
 import { Component } from '@angular/core';
+import { MainSection } from '../../model/mainsection';
+import { LocalStorageUtils } from 'src/app/utils/localstorage';
+import { MainsectionService } from '../../services/mainsection.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-delete-mainsection',
@@ -7,4 +14,57 @@ import { Component } from '@angular/core';
 })
 export class DeleteMainsectionComponent {
 
+  mainsection: MainSection;
+  errors: any[] = [];
+  localStorageUtils = new LocalStorageUtils();
+
+  constructor(
+    private mainsectionService: MainsectionService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private toastr: ToastrService,
+    private spinner: NgxSpinnerService,
+    private translateService: TranslateService) {
+
+      this.mainsection = this.route.snapshot.data['mainsection'];
+      this.spinner.hide();
+  }
+
+  deleteMainsection() {
+    this.spinner.show();
+    this.mainsectionService.deleteMainsection(this.mainsection.id)
+      .subscribe(
+        event => { this.successExclusion(event)},
+        error => { this.failDelete(error) }
+      );
+  }
+
+  successExclusion(event: any) {
+    const toast = this.toastr.success(
+      this.translateService.instant('br_com_supermarket_MAIN_SECTION_DELETE_SUCCESS'), 
+      this.translateService.instant('br_com_supermarket_MSG_GENERIC_SUCCESS')
+    );
+    if (toast) {
+      toast.onHidden.subscribe(() => {
+        this.router.navigate(['/main-section/getAll']);
+        this.spinner.hide()
+      });
+    }
+  }
+
+  failDelete(fail) {
+    if (fail.error !== null && fail.error !== undefined) {
+      this.errors = fail.error.errors;
+    } else {
+      if (fail.status === 403) {
+        this.errors = [this.translateService.instant('br_com_supermarket_LOGIN_AN_ERROR_OCCURRED_EXPIRED_LOGIN')];
+        this.localStorageUtils.clearUserLocationData();
+        this.router.navigate(['/account/login']);
+      } else {
+        this.errors = fail.message;
+      }
+    }
+    this.toastr.error(this.errors.toString(), this.translateService.instant('br_com_supermarket_MSG_ERROR'));
+    this.spinner.hide();
+  }
 }
