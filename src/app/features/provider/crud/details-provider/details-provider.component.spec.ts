41db5c873935d34bc07adc41b1c9eb64f495b7d4
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-07-08 18:50:31 -0300

    #feature/46 - Implementação do read e details de provider

--- src/app/features/provider/crud/details-provider/details-provider.component.spec.ts
+++ src/app/features/provider/crud/details-provider/details-provider.component.spec.ts
@@ -1,14 +1,38 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsProviderComponent } from './details-provider.component';
+import { ActivatedRoute } from '@angular/router';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
+import { CpfPipe, CnpjPipe } from 'src/app/utils/pipe/document';
+import { CellPhonePipe, PhonePipe } from 'src/app/utils/pipe/phone';
 
 describe('DetailsProviderComponent', () => {
   let component: DetailsProviderComponent;
   let fixture: ComponentFixture<DetailsProviderComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailsProviderComponent ]
+      declarations: [ 
+        DetailsProviderComponent,
+        CpfPipe,
+        CnpjPipe,
+        CellPhonePipe,
+        PhonePipe 
+      ],
+      providers: [ 
+        HttpClient, 
+        HttpHandler,
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ],
     })
     .compileComponents();
 
