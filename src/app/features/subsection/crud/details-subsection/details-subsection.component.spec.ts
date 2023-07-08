f1e4daed3ed333d8fd7ab8c817785b9f59948ba7
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-07-08 14:19:26 -0300

    #feature/42 - Implementação de Sub section details

--- src/app/features/subsection/crud/details-subsection/details-subsection.component.spec.ts
+++ src/app/features/subsection/crud/details-subsection/details-subsection.component.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsSubsectionComponent } from './details-subsection.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('DetailsSubsectionComponent', () => {
   let component: DetailsSubsectionComponent;
   let fixture: ComponentFixture<DetailsSubsectionComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailsSubsectionComponent ]
+      declarations: [ 
+        DetailsSubsectionComponent 
+      ],
+      providers: [ 
+        HttpClient, 
+        HttpHandler,
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ],
     })
     .compileComponents();
 
