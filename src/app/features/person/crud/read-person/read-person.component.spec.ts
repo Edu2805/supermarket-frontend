443db4fa6b7f5a43a14bdb052c7dfd5193a0fdd0
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-08-19 09:50:08 -0300

    #feature/58 - Implementação da tela de listar pessoas

--- src/app/features/person/crud/read-person/read-person.component.spec.ts
+++ src/app/features/person/crud/read-person/read-person.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReadPersonComponent } from './read-person.component';
+import { PersonService } from '../../services/person.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('ReadPersonComponent', () => {
   let component: ReadPersonComponent;
@@ -8,7 +12,23 @@ describe('ReadPersonComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReadPersonComponent ]
+      declarations: [ 
+        ReadPersonComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        PersonService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
--- src/app/features/person/crud/read-person/read-person.component.ts
+++ src/app/features/person/crud/read-person/read-person.component.ts
@@ -1,10 +1,70 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Person } from '../../model/person';
+import { Page } from 'src/app/utils/pagination/model/models';
+import { LocalStorageUtils } from 'src/app/utils/localstorage';
+import { PersonService } from '../../services/person.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-read-person',
   templateUrl: './read-person.component.html',
   styleUrls: ['./read-person.component.scss']
 })
-export class ReadPersonComponent {
+export class ReadPersonComponent implements OnInit {
 
+  public people: Array<Person> = [];
+  public page: Page<Person>;
+  PAGE = 0;
+  SIZE = 10;
+  errorMessage: string;
+  errors: any[] = [];
+  localStorageUtils = new LocalStorageUtils();
+
+  constructor(private personService: PersonService,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService,
+    private translateService: TranslateService,
+    private router: Router,) { }
+
+  ngOnInit(): void {
+    this.getAllPaged(this.PAGE, this.SIZE);
+  }
+
+  getAllPaged(page, size) {
+    this.spinner.show();
+    this.personService.getAllPeoplePaged(page, size)
+    .subscribe(
+      person => {
+        this.page = person;
+        this.people = this.page['content'];
+        this.spinner.hide();
+      },
+      fail => { 
+        this.processFail(fail) 
+      }
+    );
+  }
+
+  processFail(fail: any) {
+    if (fail.error !== null && fail.error !== undefined) {
+      this.errors = fail.error.errors;
+    } else {
+      if (fail.status === 403) {
+        this.errors = [this.translateService.instant('br_com_supermarket_LOGIN_AN_ERROR_OCCURRED_EXPIRED_LOGIN')];
+        this.localStorageUtils.clearUserLocationData();
+        this.router.navigate(['/account/login']);
+      } else {
+        this.errors = fail.message;
+      }
+    }
+    this.toastr.error(this.errors.toString(), this.translateService.instant('br_com_supermarket_MSG_ERROR'));
+    this.spinner.hide();
+  }
+
+  changePage(event){
+    this.getAllPaged(event.page, event.size);
+   }
 }
