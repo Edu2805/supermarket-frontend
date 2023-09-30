6bdfb78aa01379b04a517073ad0e8d144eb93643
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-09-30 11:55:32 -0300

    #feature/74 - Implementação da lista de salários

--- src/app/features/salary/crud/read-salary/read-salary.component.spec.ts
+++ src/app/features/salary/crud/read-salary/read-salary.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ReadSalaryComponent } from './read-salary.component';
+import { SalaryService } from '../../services/salary.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('ReadSalaryComponent', () => {
   let component: ReadSalaryComponent;
@@ -8,7 +12,23 @@ describe('ReadSalaryComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ReadSalaryComponent ]
+      declarations: [ 
+        ReadSalaryComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        SalaryService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
--- src/app/features/salary/crud/read-salary/read-salary.component.ts
+++ src/app/features/salary/crud/read-salary/read-salary.component.ts
@@ -1,10 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Salary } from '../../model/salary';
+import { Page } from 'src/app/utils/pagination/model/models';
+import { LocalStorageUtils } from 'src/app/utils/localstorage';
+import { SalaryService } from '../../services/salary.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-read-salary',
   templateUrl: './read-salary.component.html',
   styleUrls: ['./read-salary.component.scss']
 })
-export class ReadSalaryComponent {
+export class ReadSalaryComponent implements OnInit {
+
+  public salaries: Array<Salary> = [];
+  public page: Page<Salary>;
+  PAGE = 0;
+  SIZE = 10;
+  errorMessage: string;
+  errors: any[] = [];
+  localStorageUtils = new LocalStorageUtils();
+
+  constructor(private salaryService: SalaryService,
+    private spinner: NgxSpinnerService,
+    private toastr: ToastrService,
+    private translateService: TranslateService,
+    private router: Router,) { }
+
+  ngOnInit(): void {
+    this.getAllPaged(this.PAGE, this.SIZE);
+  }
+
+  getAllPaged(page, size) {
+    this.spinner.show();
+    this.salaryService.getAllSalaryPaged(page, size)
+    .subscribe(
+      salary => {
+        this.page = salary;
+        this.salaries = this.page['content'];
+        this.spinner.hide();
+      },
+      fail => { 
+        this.processFail(fail) 
+      }
+    );
+  }
+
+  processFail(fail: any) {
+    if (fail.error !== null && fail.error !== undefined) {
+      this.errors = fail.error.errors;
+    } else {
+      if (fail.status === 403) {
+        this.errors = [this.translateService.instant('br_com_supermarket_LOGIN_AN_ERROR_OCCURRED_EXPIRED_LOGIN')];
+        this.localStorageUtils.clearUserLocationData();
+        this.router.navigate(['/account/login']);
+      } else {
+        this.errors = fail.message;
+      }
+    }
+    this.toastr.error(this.errors.toString(), this.translateService.instant('br_com_supermarket_MSG_ERROR'));
+    this.spinner.hide();
+  }
+
+  changePage(event){
+    this.getAllPaged(event.page, event.size);
+   }
 
 }
