9d75580718097d357bfe273e3ed4352a22a27bbf
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-10-12 14:40:31 -0300

    #feature/157 - Implementação da estrutura básica de jobposition

--- src/app/features/jobposition/services/jobposition.service.spec.ts
+++ src/app/features/jobposition/services/jobposition.service.spec.ts
@@ -1,12 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { JobPositionService } from './jobposition.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser, MissingTranslationHandler } from '@ngx-translate/core';
 
 describe('JobpositionService', () => {
   let service: JobPositionService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        JobPositionService,
+        HttpClient,
+        HttpHandler,
+        TranslateService,
+        TranslateStore,
+        TranslateLoader,
+        TranslateCompiler,
+        TranslateParser,
+        MissingTranslationHandler 
+      ]
+    });
     service = TestBed.inject(JobPositionService);
   });
 
