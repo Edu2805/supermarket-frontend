1d3fe27d4aefec2a901a71932ae34e460063e4cd
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-10-12 18:42:24 -0300

    #feature/159 - Implementação da tela de atualização do cargo

--- src/app/features/jobposition/crud/update-jobposition/update-jobposition.component.spec.ts
+++ src/app/features/jobposition/crud/update-jobposition/update-jobposition.component.spec.ts
@@ -1,14 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { UpdateJobpositionComponent } from './update-jobposition.component';
+import { JobPositionService } from '../../services/jobposition.service';
+import { SalaryService } from 'src/app/features/salary/services/salary.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ActivatedRoute } from '@angular/router';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('UpdateJobpositionComponent', () => {
   let component: UpdateJobpositionComponent;
   let fixture: ComponentFixture<UpdateJobpositionComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UpdateJobpositionComponent ]
+      declarations: [ 
+        UpdateJobpositionComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        JobPositionService,
+        SalaryService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
