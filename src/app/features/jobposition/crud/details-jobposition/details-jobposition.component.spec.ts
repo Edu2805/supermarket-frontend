d767ebe60b2b97299c2e492f76319d58da57cbe1
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-10-12 22:08:08 -0300

    #feature/78 - Implementação das telas de exclusão e detalhes do cargo

--- src/app/features/jobposition/crud/details-jobposition/details-jobposition.component.spec.ts
+++ src/app/features/jobposition/crud/details-jobposition/details-jobposition.component.spec.ts
@@ -1,14 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DetailsJobpositionComponent } from './details-jobposition.component';
+import { ActivatedRoute } from '@angular/router';
+import { JobPositionService } from '../../services/jobposition.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('DetailsJobpositionComponent', () => {
   let component: DetailsJobpositionComponent;
   let fixture: ComponentFixture<DetailsJobpositionComponent>;
+  const fakeActivatedRoute = {
+    snapshot: { data: { } }
+  } as ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DetailsJobpositionComponent ]
+      declarations: [ 
+        DetailsJobpositionComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        JobPositionService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+        {provide: ActivatedRoute, useValue: fakeActivatedRoute}
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ],
     })
     .compileComponents();
 
--- src/app/features/jobposition/crud/details-jobposition/details-jobposition.component.ts
+++ src/app/features/jobposition/crud/details-jobposition/details-jobposition.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { JobPosition } from '../../model/jobposition';
+import { LocalStorageUtils } from 'src/app/utils/localstorage';
+import { ActivatedRoute } from '@angular/router';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-details-jobposition',
@@ -7,4 +11,15 @@ import { Component } from '@angular/core';
 })
 export class DetailsJobpositionComponent {
 
+  jobPosition: JobPosition;
+  errors: any[] = [];
+  localStorageUtils = new LocalStorageUtils();
+
+  constructor(
+    private route: ActivatedRoute,
+    private spinner: NgxSpinnerService) {
+
+      this.jobPosition = this.route.snapshot.data['jobposition'];
+      this.spinner.hide();
+  }
 }
