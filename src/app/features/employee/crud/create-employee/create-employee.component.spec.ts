24c5f458500f88067684e65087aca0b2bf3b0772
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2023-10-15 13:23:44 -0300

    #feature/80 - Implementação da estrutura básica de employee

--- src/app/features/employee/crud/create-employee/create-employee.component.spec.ts
+++ src/app/features/employee/crud/create-employee/create-employee.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateEmployeeComponent } from './create-employee.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
+import { AccountService } from 'src/app/features/account/services/account.service';
+import { EmployeeService } from '../../services/employee.service';
 
 describe('CreateEmployeeComponent', () => {
   let component: CreateEmployeeComponent;
@@ -8,7 +13,24 @@ describe('CreateEmployeeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateEmployeeComponent ]
+      declarations: [ 
+        CreateEmployeeComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        EmployeeService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+        AccountService
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
--- src/app/features/employee/services/employee.service.spec.ts
+++ src/app/features/employee/services/employee.service.spec.ts
@@ -1,12 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { EmployeeService } from './employee.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser, MissingTranslationHandler } from '@ngx-translate/core';
 
 describe('EmployeeService', () => {
   let service: EmployeeService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        EmployeeService,
+        HttpClient,
+        HttpHandler,
+        TranslateService,
+        TranslateStore,
+        TranslateLoader,
+        TranslateCompiler,
+        TranslateParser,
+        MissingTranslationHandler 
+      ]
+    });
     service = TestBed.inject(EmployeeService);
   });
 
