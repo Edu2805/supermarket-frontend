17dca1a189965e5aa7b466b8a394d4f79e2dc22b
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2024-01-03 16:02:02 -0300

    #feature/90 - Implementação da estrutura básica de Goods Issue e renomeação de pacotes

--- src/app/features/goodsissue/feature/create-goods-issue/create-goods-issue.component.spec.ts
+++ src/app/features/goodsissue/feature/create-goods-issue/create-goods-issue.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateGoodsIssueComponent } from './create-goods-issue.component';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
+import { GoodsIssueGuardService } from '../../services/goodsissue.guard';
 
 describe('CreateGoodsIssueComponent', () => {
   let component: CreateGoodsIssueComponent;
@@ -8,7 +12,23 @@ describe('CreateGoodsIssueComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CreateGoodsIssueComponent ]
+      declarations: [ 
+        CreateGoodsIssueComponent 
+      ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        GoodsIssueGuardService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
--- src/app/features/goodsissue/services/goodsissue.service.spec.ts
+++ src/app/features/goodsissue/services/goodsissue.service.spec.ts
@@ -1,12 +1,30 @@
 import { TestBed } from '@angular/core/testing';
 
 import { GoodsissueService } from './goodsissue.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateStore, TranslateLoader, TranslateCompiler, TranslateParser, MissingTranslationHandler } from '@ngx-translate/core';
 
 describe('GoodsissueService', () => {
   let service: GoodsissueService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        GoodsissueService,
+        HttpClient,
+        HttpHandler,
+        TranslateService,
+        TranslateStore,
+        TranslateLoader,
+        TranslateCompiler,
+        TranslateParser,
+        MissingTranslationHandler 
+      ]
+    });
     service = TestBed.inject(GoodsissueService);
   });
 
