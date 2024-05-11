376b4fdc60d76a94a67a481f716300409778493e
Eduardo <eduardo@MacBook-Air-de-Eduardo.local>
2024-05-11 16:11:12 -0300

    #feature/100 - Implementação dos componentes base para os relatórios financeiros

--- src/app/features/financial-statement-reports/reports/purchases-reports/purchases-reports.component.spec.ts
+++ src/app/features/financial-statement-reports/reports/purchases-reports/purchases-reports.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PurchasesReportsComponent } from './purchases-reports.component';
+import { FinancialStatementReportsGuardService } from '../../services/financial-statement-reports.guard';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('PurchasesReportsComponent', () => {
   let component: PurchasesReportsComponent;
@@ -8,7 +12,21 @@ describe('PurchasesReportsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PurchasesReportsComponent ]
+      declarations: [ PurchasesReportsComponent ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        FinancialStatementReportsGuardService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
--- src/app/features/financial-statement-reports/reports/sales-reports/sales-reports.component.spec.ts
+++ src/app/features/financial-statement-reports/reports/sales-reports/sales-reports.component.spec.ts
@@ -1,6 +1,10 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SalesReportsComponent } from './sales-reports.component';
+import { FinancialStatementReportsGuardService } from '../../services/financial-statement-reports.guard';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { USE_DEFAULT_LANG, USE_STORE, USE_EXTEND, DEFAULT_LANGUAGE, TranslateService, TranslateModule } from '@ngx-translate/core';
+import { ToastrModule } from 'ngx-toastr';
 
 describe('SalesReportsComponent', () => {
   let component: SalesReportsComponent;
@@ -8,7 +12,21 @@ describe('SalesReportsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SalesReportsComponent ]
+      declarations: [ SalesReportsComponent ],
+      providers: [ 
+        { provide: USE_DEFAULT_LANG, useValue: undefined },
+        { provide: USE_STORE, useValue: undefined },
+        { provide: USE_EXTEND, useValue: undefined },
+        { provide: DEFAULT_LANGUAGE, useValue: undefined },
+        FinancialStatementReportsGuardService,
+        HttpClient, 
+        HttpHandler,
+        TranslateService,
+      ],
+      imports: [
+        ToastrModule.forRoot(),
+        TranslateModule.forRoot(), 
+      ]
     })
     .compileComponents();
 
